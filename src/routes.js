2d7dac3626a49b4f5750a9d1e8c28e5996092ce4
rodrigo-os <rodrigo.os743@gmail.com>
2023-01-31 15:32:06 -0300

    Definição da rota referente a atualização de uma viagem, correção na rota de listagem.

--- src/routes.js
+++ src/routes.js
@@ -19,10 +19,36 @@ router.post("/travel", (request, response) =>{
 });
 
 // Read
-router.get("/travel", (request, response) =>{
+router.get("/travels", (request, response) =>{
     database.find({ }, (err, docs) =>{
         return err?console.log(err):response.status(200).json(docs);
-    })
-})
+    });
+});
+
+// Update
+router.put("/travel", (request, response) =>{
+    const _id = request.body._id;
+    const travel = {
+        from:{
+            city:request.body.from.city,
+            state:request.body.from.state,
+        },
+        to:{
+            city:request.body.to.city,
+            state:request.body.to.state,
+        },
+        travel_duration:request.body.travel_duration,
+        travel_distance:request.body.travel_distance,
+    };
+    return !_id
+        ? response.status(400).json("Identificador não informado!")
+        : database.findOne({_id}, (err,doc) =>{
+            !doc
+                ? response.status(404).json("O Identificador informado não foi encontrado!")
+                : database.update({_id}, {travel}, {}, (err) =>{
+                    return err?console.log(err):response.status(201).json(travel);
+                })
+        })
+});
 
 module.exports = router;
